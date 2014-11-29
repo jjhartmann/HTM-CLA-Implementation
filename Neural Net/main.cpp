983f8f2e6bbff787c55aa86726ee9b246d387091
Jeremy J Hartmann <jhartman@sfu.ca>
2014-11-29 12:08:56 -0800

    Initial Commit

---
+++ Neural Net/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Neural Net
+//
+//  Created by Jeremy Hartmann on 2014-11-29.
+//  Copyright (c) 2014 Jeremy Hartmann. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
